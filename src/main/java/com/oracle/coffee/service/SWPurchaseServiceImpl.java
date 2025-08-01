d74c799a2313ef4acccd6a8d3f96bfb856440311
psw990802 <samuel3434@naver.com>
2025-08-01 17:00:31 +0900

    8/1 17:00

--- src/main/java/com/oracle/coffee/service/SWPurchaseServiceImpl.java
+++ src/main/java/com/oracle/coffee/service/SWPurchaseServiceImpl.java
@@ -3,9 +3,7 @@ package com.oracle.coffee.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.oracle.coffee.dao.ClientMapper;
 import com.oracle.coffee.dao.SWPurchaseDao;
 import com.oracle.coffee.dto.PurchaseDto;
 
@@ -13,14 +11,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Service
-//@Transactional
 @Log4j2
 @RequiredArgsConstructor
 public class SWPurchaseServiceImpl implements SWPurchaseService {
 	private final SWPurchaseDao		swPurchaseDao;
-	private final ClientMapper      clientMapper;
 
-	// 원본 
 	@Override
 	public int purchaseSave(PurchaseDto purchaseDto) {
 		System.out.println("SWPurchaseServiceImpl purchaseSave start...");
@@ -30,21 +25,6 @@ public class SWPurchaseServiceImpl implements SWPurchaseService {
 		return purchase_code;
 	}
 	
-	@Transactional
-	@Override
-	public int purchaseSavetest(PurchaseDto purchaseDto) {
-		System.out.println("SWPurchaseServiceImpl purchaseSave start...");
-        clientMapper.purchaseSave(purchaseDto);
-        System.out.println("SWPurchaseServiceImpl purchaseDto=>"+purchaseDto);
-        // 예외 발생 시 전체 롤백
-        if (clientMapper.getClass()== null) {
-            throw new IllegalArgumentException("purchaseSave not be null");
-        }
-        purchaseDto.setPurchase_code(1);
-        clientMapper.purchaseDetailSave(purchaseDto);		
-		return 1;
-	}
-
 	@Override
 	public int totalPurchaseCnt() {
 		System.out.println("SWPurchaseServiceImpl totalPurchaseCnt start...");
