e620e347f42c987d04f7b29ea9f1e6d21ecbcf35
sanghang33 <harrytop@naver.com>
2025-08-22 12:49:53 +0900

    mfg 수정

--- src/main/java/com/oracle/coffee/configuration/SecurityConfig.java
+++ src/main/java/com/oracle/coffee/configuration/SecurityConfig.java
@@ -41,48 +41,51 @@ public class SecurityConfig {
         	.authorizeHttpRequests(auth->auth
                     .requestMatchers( "/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*"
                     	         	, "/WEB-INF/views/**").permitAll()
+                    //login
                     .requestMatchers("/","/login*","/error*").permitAll()
                     .requestMatchers("/login/findPassword","/login/findPassword/**").permitAll()
+                    //client
                     .requestMatchers("/client/clientInForm").hasAnyAuthority("ROLE_USER","ROLE_MANAGER")
                     .requestMatchers("/client/**").hasAnyAuthority("ROLE_USER", "ROLE_MANAGER")
+                    //emp
                     .requestMatchers("/emp/empInForm").hasAuthority("ROLE_MANAGER")
                     .requestMatchers("/emp/empDetail**").hasAuthority("ROLE_MANAGER")
                     .requestMatchers("/emp/**").hasAnyAuthority("ROLE_USER", "ROLE_MANAGER")
+                    //dept
                     .requestMatchers("/dept/deptInForm").hasAuthority("ROLE_MANAGER")
-                    .requestMatchers("/MyPage/MyPage").authenticated() //마이페이지는 로그인 필요
-                    //->특정 페이지에 특정 ROLE을 가진사람만 접근가능하게 하고 싶을 경우 
-                    
+                    //mypage
+                    .requestMatchers("/MyPage/MyPage").authenticated() 
+                    //provide
                     .requestMatchers("/provide/provideInForm").hasAuthority("ROLE_MANAGER")
-                    
+                    //sw
                     .requestMatchers("/sw/wonProductInForm").hasAuthority("ROLE_MANAGER")
-                    
-                    .requestMatchers("/km/productInForm").hasAuthority("ROLE_MANAGER")
-                    .requestMatchers("/km/mfgInForm").hasAuthority("ROLE_MANAGER")
-                    .requestMatchers("/km/mfgReportList").hasAuthority("ROLE_MANAGER")
-                    .requestMatchers("/board/boardList").hasAuthority("ROLE_MANAGER")
-                    
+                    //km
+                    .requestMatchers("/km/productInForm").hasAnyAuthority("ROLE_USER","ROLE_MANAGER")
+                    .requestMatchers("/km/mfgInForm").hasAnyAuthority("ROLE_USER","ROLE_MANAGER")
+                    .requestMatchers("/km/mfgReportList").hasAnyAuthority("ROLE_USER","ROLE_MANAGER")
+                    .requestMatchers("/km/wanList").hasAnyAuthority("ROLE_USER","ROLE_MANAGER")
+                    .requestMatchers("/km/mfgList").hasAnyAuthority("ROLE_USER","ROLE_MANAGER")
+                    //board
+                    .requestMatchers("/board/boardEditForm/**").hasAuthority("ROLE_MANAGER")
+                    .requestMatchers("/board/boardWriteForm").hasAnyAuthority("ROLE_USER","ROLE_MANAGER")
+                    //order
                     .requestMatchers("/order/new").hasAuthority("ROLE_CLIENT2")
                     .requestMatchers("/order/approve/**").hasAuthority("ROLE_MANAGER")
                     .requestMatchers("/order/**").hasAnyAuthority("ROLE_USER", "ROLE_MANAGER", "ROLE_CLIENT2")
-                    
+                    //inventory
                     .requestMatchers("/inventory/**").hasAnyAuthority("ROLE_USER", "ROLE_MANAGER")
                    
                     
-                    //통합기간중에만 login불필요
-                    //아래 매핑으로 시작되는 url은 로그인 필요없이 접근 가능
                     
-                    .requestMatchers("/jh/**").permitAll()
-                    .requestMatchers("/sw/**").permitAll()
-                    //.requestMatchers("/provide/**").permitAll()
-                    .requestMatchers("/km/**").permitAll()
-                    
-                    
+                    //Login 불필요
+                    .requestMatchers("/sw/**").permitAll()                    
+                    //.requestMatchers("/km/**").permitAll()
+                    .requestMatchers("/board/**").permitAll()
                     
 				    .anyRequest().authenticated()
 				  	
 					)
 			
-			// 
         	.formLogin(form -> form
         		    .loginPage("/login").permitAll()
         		    .loginProcessingUrl("/login")            
